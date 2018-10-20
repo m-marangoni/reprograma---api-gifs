c393357bdd1b515b01f15aa0845f2494aa2dd9ae
Mariana <marangoni.mariana@gmail.com>
2018-10-19 22:30:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btnBusca = document.getElementById("btn-busca");
 btnBusca.addEventListener("click", trazResultadoDaBusca);
 let listaGifs = [];
 
-function buscaGif(){
+function buscaPalavra(){
   return document.getElementById("campo-busca").value;
 }
 
@@ -13,29 +13,28 @@ function erro(){
 function trazResultadoDaBusca(event){
   event.preventDefault();
 const respostaBusca = new XMLHttpRequest();
-respostaBusca.open("GET", `http://api.giphy.com/v1/gifs/search?q=${buscaGif()}&api_key=tnUfJpgxZmbgV9VrgRQ3rGKJBeCAx1Re&limit=1`);
-respostaBusca.onload = carregaPostsComGifs;
+respostaBusca.open("GET", `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${buscaPalavra()}&api-key=0f8b883df781451390110137a2fd9e96`);
+respostaBusca.onload = carregaPosts;
 respostaBusca.onerror = erro;
 respostaBusca.send();
-
-
 }
 
-function carregaPostsComGifs(){
-  listaGifs = JSON.parse(this.responseText)["data"];
+function carregaPosts(){
+  docs = JSON.parse(this.responseText)["response"]["docs"];
   exibePosts();
 }
 
 function exibePosts(){
   let exibeBusca = document.getElementById("exibe-busca");
     exibeBusca.innerHTML = 
-      `<div class="area-gif"> ${listaGifs.map(gif => `
-        <div class="gif">
-          <img src="${gif.images.fixed_height.url}"></img>
+      `<div class="area-noticia"> ${docs.map(doc => `
+        <div class="noticia">
+          <h3>${doc.headline.main}</h3>
+          
         </div>
         `).join("")}
       </div>`;
 }
-  
+   
 
 
